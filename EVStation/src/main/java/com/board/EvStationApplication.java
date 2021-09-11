105ade7b8195e781d996525b99161cfe41efc160
pyjavathon <ayeonyun391@gmail.com>
2021-09-12 02:31:12 +0900

    커밋

--- EVStation/src/main/java/com/board/EvStationApplication.java
+++ EVStation/src/main/java/com/board/EvStationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EVStationApplication {
+public class EvStationApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(EVStationApplication.class, args);
+		SpringApplication.run(EvStationApplication.class, args);
 	}
 
 }
